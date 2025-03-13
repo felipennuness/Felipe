c2fdb55e380f9c56e43b28e8cff1834f0630b5e9
LUIZ FELIPE NUNES DA SILVA MONTEIRO <luiz.monteiro00@cs.cruzeirodosul.edu.br>
2025-03-12 21:22:19 -0300

    atualizando arquivo

--- Felipe.js
+++ Felipe.js
@@ -3,3 +3,15 @@ console.log("5" - 3)//2
 console.log("5"+1)//51
 console.log("dois" * "três")
 console.log(`A soma de 2 + 2 é ${2 + 2}`)
+
+
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Qual é o seu nome:?', nome => {
+    console.log(`Olá, ${nome}!`);
+    readline.close();
+});
